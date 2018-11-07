22d9bb4bb233a3c5a9ffc59e6c79ff4463e6a53b
pengwei <15110235514@163.com>
2018-11-07 11:23:01 +0800

    创建MainProject

---
+++ MainProject/MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by pengwei on 2018/11/7.
+//  Copyright © 2018 pengwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
